b777518ceaacd29be4882591f0f34b20125b8589
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2022-10-25 22:06:27 +0900

    Install the Apollo frameworks

--- Package.swift
+++ Package.swift
@@ -4,25 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Apollo-ios-tutorial",
+    name: "InternalLibrary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Apollo-ios-tutorial",
-            targets: ["Apollo-ios-tutorial"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apollographql/apollo-ios.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Apollo-ios-tutorial",
-            dependencies: []),
-        .testTarget(
-            name: "Apollo-ios-tutorialTests",
-            dependencies: ["Apollo-ios-tutorial"]),
+            name: "FeatureA",
+            dependencies: [
+                .product(name: "Apollo", package: "apollo-ios"),
+            ]),
     ]
 )
