5ec6ee8f3e82ce3655fb483c8b9af604af46fc0d
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2022-10-25 21:29:59 +0900

    init xcode project

---
+++ Apollo-ios-tutorial/Apollo_ios_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apollo_ios_tutorialApp.swift
+//  Apollo-ios-tutorial
+//
+//  Created by 阿久津　岳志 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Apollo_ios_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apollo-ios-tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apollo-ios-tutorial
+//
+//  Created by 阿久津　岳志 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
